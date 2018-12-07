67af72183a288e27f112886a23378acd0c627f35
Otavio Zabaleta <ozabalet@ford.com>
2018-12-07 14:31:49 +0000

    cookies

--- Public/scripts/showResolution.js
+++ Public/scripts/showResolution.js
@@ -1,10 +1,10 @@
 function toggleSolution() {
     var resolutionDiv = document.getElementById("resolution");
-    resolutionDiv.hidden = !resolutionDiv.hidden
+    resolutionDiv.hidden = !resolutionDiv.hidden;
     var toggleButton = document.getElementById("toggleButton");
     if(resolutionDiv.hidden) {
-      toggleButton.innerHTML = "Show Resolution"
+      toggleButton.innerHTML = "Show Resolution";
     } else {
-      toggleButton.innerHTML = "Hide Resolution"
+      toggleButton.innerHTML = "Hide Resolution";
     }
 }
--- Sources/App/WebsiteControllers/IndexWebsiteController.swift
+++ Sources/App/WebsiteControllers/IndexWebsiteController.swift
@@ -23,17 +23,26 @@ struct IndexWebsiteController: RouteCollection {
 private extension IndexWebsiteController {
     func indexHandler(req: Request) throws -> Future<View> {
         return List.query(on: req).filter(\.name == "Shop List").first().flatMap(to: View.self) { maybeList in
+            
+            let showCookies = req.http.cookies["cookies-accepted"] == nil
+            
             // Check is "Shop List" is already created
             guard let list = maybeList, let listID = list.id else {
                 // If not, create and render it
                 return List(name: "Shop List").save(on: req).flatMap(to: View.self) { list in
-                    return try req.view().render("singlelist.leaf", IndexContext(title: list.name, listID: list.requireID(), items: nil))
+                    return try req.view().render("singlelist.leaf", IndexContext(title: list.name,
+                                                                                 listID: list.requireID(),
+                                                                                 items: nil,
+                                                                                 showCookiesMessage: showCookies))
                 }
             }
             
             // If so, render it
             return try list.items.query(on: req).all().flatMap(to: View.self) { items in
-                return try req.view().render("singlelist.leaf", IndexContext(title: list.name, listID: listID, items: items))
+                return try req.view().render("singlelist.leaf", IndexContext(title: list.name,
+                                                                             listID: listID,
+                                                                             items: items,
+                                                                             showCookiesMessage: showCookies))
             }
         }
     }
@@ -43,4 +52,5 @@ struct IndexContext: Encodable {
     let title: String
     let listID: Int
     let items: [Item]?
+    let showCookiesMessage: Bool
 }
