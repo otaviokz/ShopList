ed1633211503e368c8cb1866e7be8587e8f9c487
Otavio Zabaleta <ozabalet@ford.com>
2018-12-11 14:07:22 +0000

    UI fixes

--- Public/scripts/cookies.js
+++ Public/scripts/cookies.js
@@ -1,7 +1,7 @@
 function cookiesConfirmed() {
   $("#cookie-footer").hide();
-  var d = new Date();
-  d.setTime(d.getTime() + (365*24*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = "cookies-accepted=true;" + expires;
+  var date = new Date();
+  date.setTime(d.getTime() + (365*24*60*60*1000));
+  var expirationString = "expires=" + date.toUTCString();
+  document.cookie = "cookies-accepted=true;" + expirationString;
 }
